9cb20bfe7dab3533dce6d2c14744275e0d26a695
yana-shapka <57398010+yana-shapka@users.noreply.github.com>
2025-01-24 23:40:45 +0000

    Deploying to gh-pages from @ Kate2603/PowerPixel@db036b90ee8c50a3e6b104650fc12d811cf15eef 🚀

---
+++ favorites.js
@@ -0,0 +1,10 @@
+import"./assets/scroll-up-a2752034.js";import"./assets/vendor-cc1203f3.js";function r(){const t=document.querySelector(".favorites-list"),e=document.querySelector(".no-favorites-message"),o=JSON.parse(localStorage.getItem("favorites"))||[];t.innerHTML="",o.length===0?(e.style.display="block",t.style.display="none"):(e.style.display="none",t.style.display="block",o.forEach((n,s)=>{const a=i(n,s);t.appendChild(a)}))}function i(t,e){const o=document.createElement("li");return o.classList.add("exercise-card"),o.innerHTML=`
+    <div class="exercise-info">
+      <h3>${t.name}</h3>
+      <p><strong>Calories:</strong> ${t.calories} / 3 min</p>
+      <p><strong>Body Part:</strong> ${t.bodyPart}</p>
+      <p><strong>Target:</strong> ${t.target}</p>
+    </div>
+    <button class="remove-button">Remove</button>
+  `,o.querySelector(".remove-button").addEventListener("click",()=>{c(e)}),o}function c(t){const e=JSON.parse(localStorage.getItem("favorites"))||[];e.splice(t,1),localStorage.setItem("favorites",JSON.stringify(e)),r()}function l(){r()}document.addEventListener("DOMContentLoaded",l);
+//# sourceMappingURL=favorites.js.map
