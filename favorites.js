d4ec4d9e0550015bd383df3889d0ae2fabd3b7a1
yana-shapka <57398010+yana-shapka@users.noreply.github.com>
2025-01-26 14:00:51 +0000

    Deploying to gh-pages from @ Kate2603/PowerPixel@92ef5f8f7750eca7d7fa9f719b8daeda5d4b03d3 🚀

--- favorites.js
+++ favorites.js
@@ -1,15 +1,14 @@
-import{r}from"./assets/scroll-up-472b6995.js";import"./assets/vendor-cc1203f3.js";function n(){const t=document.querySelector(".favorites-list"),s=document.querySelector(".no-favorites-message"),e=JSON.parse(localStorage.getItem("favorites"))||[];t.innerHTML="",e.length===0?(s.style.display="block",t.style.display="none"):(s.classList.add("hidden"),e.forEach((a,o)=>{const i=d(a);t.appendChild(i)}))}function d(t,s){const e=document.createElement("li");return e.classList.add("exercise-card"),e.innerHTML=`<li>
+import{r as n}from"./assets/scroll-up-cb154631.js";import"./assets/vendor-cc1203f3.js";function r(){const t=document.querySelector(".favorites-list"),s=document.querySelector(".no-favorites-message"),e=JSON.parse(localStorage.getItem("favorites"))||[];t.innerHTML="",e.length===0?(s.style.display="block",t.style.display="none"):(s.classList.add("hidden"),e.forEach((a,o)=>{const i=d(a);t.appendChild(i)}))}function d(t,s){const e=document.createElement("li");return e.classList.add("exercise-card"),e.innerHTML=`<li>
   <div class="filtered-exercises-categories-list-item">
   <p class="workout"> Workout
     </p>
-  <div class="remove-icon">
-  <button class="remove-button" data-id="${t.id}">Remove
-  <svg class="icon" aria-hidden="true" width="24" height="24">
+  <button class="remove-button" data-id="${t.id}">
+  <svg class="remove-icon" aria-hidden="true" width="24" height="24">
   <use href="./img/sprite.svg#basket"></use>
-</svg></button></div>
+</svg></button>
   <button class="start-button" data-id="${t.id}">Start
-    <svg class="icon" aria-hidden="true" width="24" height="24">
-      <use href="./img/sprite.svg#icon-arrow-single-right"></use>
+    <svg class="icon-arrow" aria-hidden="true" width="24" height="24">
+      <use href="./img/sprite.svg#icon-arrow"></use>
     </svg>
   </button>
   <div class="filtered-categories-content">
@@ -24,5 +23,5 @@ import{r}from"./assets/scroll-up-472b6995.js";import"./assets/vendor-cc1203f3.js
     <p><span>Body Part:</span> ${t.bodyPart}</p>
     <p><span>Target:</span>${t.target}</p></div></div>
   </div>
-</li>`,e.querySelector(".remove-button").addEventListener("click",o=>{const i=o.target.attributes["data-id"].value;r(i),n()}),e}function c(){n()}document.addEventListener("DOMContentLoaded",c);
+</li>`,e.querySelector(".remove-button").addEventListener("click",o=>{const i=o.target.attributes["data-id"].value;n(i),r()}),e}function c(){r()}document.addEventListener("DOMContentLoaded",c);
 //# sourceMappingURL=favorites.js.map
